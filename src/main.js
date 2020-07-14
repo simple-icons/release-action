e53521b50401a0b0b768ee744234ab87e7e6a786
Eric Cornelissen <ericornelissen@gmail.com>
2020-07-14 10:04:46 +0300

    Create test suite for main

--- src/main.js
+++ src/main.js
@@ -30,3 +30,5 @@ async function main() {
 }
 
 main();
+
+module.exports = main;
---
+++ test/main.test.js
@@ -0,0 +1,30 @@
+const github = require("../__mocks__/@actions/github");
+const main = require("../src/main");
+
+jest.mock("../src/create", () => jest.fn());
+jest.mock("../src/merge", () => jest.fn());
+
+const createMock = require("../src/create");
+const mergeMock = require("../src/merge");
+
+
+beforeAll(() => {
+  createMock.mockClear();
+  mergeMock.mockClear();
+});
+
+test('event "schedule"', async () => {
+  expect.assertions(1);
+
+  github.context.eventName = "schedule";
+  await main();
+  expect(createMock).toHaveBeenCalledTimes(1);
+});
+
+test('event "pull_request_review"', async () => {
+  expect.assertions(1);
+
+  github.context.eventName = "pull_request_review";
+  await main();
+  expect(mergeMock).toHaveBeenCalledTimes(1);
+});
